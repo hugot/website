0faf4737d5a5f49ec34fa39469ad739abde29924
Hugo Thunnissen <hugo@hugot.nl>
2020-02-07 20:53:08 +0100

    Remove attempt at good high-DPI font size as it is futile :(

--- generate-blog.bash
+++ generate-blog.bash
@@ -65,12 +65,6 @@ blog_html="$here/blog.html"
              max-width: 50em;
          }
      }
-
-     @media only all and (pointer: fine) and (min-width: 2000px) {
-         body {
-             font-size: 40px;
-         }
-     }
     </style>
 
     <body>
